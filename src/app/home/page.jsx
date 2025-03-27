8057ef9f129bcd5b45e61eec6e7af494e9e03b2d
HP <chandan.workholics@gmail.com>
2025-03-27 19:08:43 +0530

    updated

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -65,16 +65,16 @@ const HomePage = () => {
                                 <Swiper
                                     slidesPerView={1}
                                     spaceBetween={10}
-                                    // autoplay={{
-                                    //     delay: 3000,
-                                    //     disableOnInteraction: false,
-                                    // }}
+                                    autoplay={{
+                                        delay: 3000,
+                                        disableOnInteraction: false,
+                                    }}
                                     breakpoints={{
                                         640: { slidesPerView: 2, spaceBetween: 20 },
                                         768: { slidesPerView: 4, spaceBetween: 40 },
                                         1024: { slidesPerView: 4, spaceBetween: 50 },
                                     }}
-                                    // modules={[Pagination, Autoplay]}
+                                    modules={[Pagination, Autoplay]}
                                     className="mySwiper"
                                 >
                                     {Array.isArray(posts?.data) ? posts.data.map((post, index) => (
